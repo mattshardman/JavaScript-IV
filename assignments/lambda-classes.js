c8a80b0431fd24436aaf3332bf9bb2b7c8be22aa
mattshardman <mattshardman@gmail.com>
2019-01-24 09:33:24 +0000

    create instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,12 +1,29 @@
 // CODE here for your Lambda Classes
 class Person {
-    constructor() {
-        this.name = name;
-        this.location = location;
-        this.gender = gender;
+    constructor(assignments) {
+        this.name = assignments.name;
+        this.location = assignments.location;
+        this.gender = assignments.gender;
     }
 
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(assignments) {
+        super(assignments);
+        this.specialty = assignments.specialty;
+        this.favLanguage = assignments.favLanguage;
+        this.catchPhrase = assignments.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
     }
 }
